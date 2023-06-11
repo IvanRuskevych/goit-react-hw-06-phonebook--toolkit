7320efb275a96e9e4168a8463f0510257d0999b1
unknown <ivan.ruskevych@gmail.com>
2023-06-11 03:45:55 +0300

    hw-06 step-01 contactsSlice & filtersSlice

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,16 @@
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 
+import { deleteContact } from 'redux/contactsSlice';
+
 import css from './Contact.module.css';
-import { useDispatch } from 'react-redux';
-import { deleteContacts } from 'redux/actions';
 
 function Contact({ item }) {
   const dispatch = useDispatch();
-  const handleDeleteContact = () => dispatch(deleteContacts(item.id));
+  const handleDeleteContact = () => dispatch(deleteContact(item.id));
 
   return (
     <li className={css.item}>
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,19 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import { getFilterValue } from 'redux/actions';
-
 import TextField from '@mui/material/TextField';
-import css from './Filter.module.css';
+
 import { getFilter } from 'redux/selectors';
+import { setFilterValue } from 'redux/filtersSlice';
+
+import css from './Filter.module.css';
 
 export default function Filter() {
   const dispatch = useDispatch();
   const handleFilterChange = e => {
-    // console.log(e.target.value);
-    dispatch(getFilterValue(e.target.value));
+    dispatch(setFilterValue(e.target.value));
   };
   const filter = useSelector(getFilter);
-  // console.log(filter);
 
   return (
     <label className={css.filter}>
