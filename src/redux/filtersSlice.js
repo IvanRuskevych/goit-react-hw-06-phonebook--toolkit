5420017e61880568606d2cb85a9c73dbceeb5cad
unknown <ivan.ruskevych@gmail.com>
2023-06-11 05:41:27 +0300

    hw-06 persist

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,4 +1,6 @@
-const { createSlice } = require('@reduxjs/toolkit');
+import { createSlice } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { persistReducer } from 'redux-persist';
 
 const filtersInitialState = {
   filter: '',
@@ -17,5 +19,15 @@ const filtersSlice = createSlice({
   },
 });
 
+const persistConfig = {
+  key: 'filter',
+  storage,
+};
+
 export const { setFilterValue } = filtersSlice.actions;
-export const filtersReducer = filtersSlice.reducer;
+// export const filtersReducer = filtersSlice.reducer;
+
+export const filtersReducer = persistReducer(
+  persistConfig,
+  filtersSlice.reducer
+);
